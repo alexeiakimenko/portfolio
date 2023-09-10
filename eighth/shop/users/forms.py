0247f011c8eaff905a8f9d4431a350dd9438d816
alexeiakimenko <alexeiakimenko@ya.ru>
2023-09-10 18:57:25 +0700

    change

--- eighth/shop/users/forms.py
+++ eighth/shop/users/forms.py
@@ -1,6 +1,9 @@
-from django.contrib.auth.forms import AuthenticationForm
-from users.models import User
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
+from .models import User, EmailVerification
 from django import forms
+import uuid
+from datetime import timedelta
+from django.utils.timezone import now
 
 
 class UserLoginForm(AuthenticationForm):
@@ -16,4 +19,50 @@ class UserLoginForm(AuthenticationForm):
 
     class Meta:
         model = User
-        fields = ('username', 'password', 'password')
+        fields = ('username', 'password')
+
+
+class UserRegistrationForm(UserCreationForm):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Введите имя'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Введите имя'}))
+    email = forms.CharField(widget=forms.EmailInput(attrs={'placeholder': 'Введите адрес эл.почты'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Введите пароль'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Подтвердите пароль'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Введите фамилию'}))
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control py-4'
+
+    def save(self, commit=True):
+        user = super().save(commit=True)
+        expiration = now() + timedelta(hours=48)
+        record = EmailVerification.objects.create(
+            code=uuid.uuid4(),
+            user=user,
+            expiration=expiration,
+
+        )
+        record.send_verification_email()
+        return user
+
+
+class UserProfileForm(UserChangeForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'readonly': True}))
+    email = forms.CharField(widget=forms.EmailInput(attrs={'readonly': True}))
+    image = forms.ImageField(widget=forms.FileInput(), required=False)
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'username', 'email', 'image')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control py-4'
+        self.fields['image'].widget.attrs['class'] = 'custom-file-input'
--- eighth/shop/users/models.py
+++ eighth/shop/users/models.py
@@ -4,3 +4,18 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     image = models.ImageField(upload_to='users_images', blank=True)
+    is_verify_email = models.BooleanField(default=False, verbose_name='Подтверждение электронной почты')
+
+
+class EmailVerification(models.Model):
+    code = models.UUIDField(unique=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    expiration = models.DateTimeField()
+
+    def __str__(self):
+        return f'EmailVerification object for {self.user.email}'
+
+    class Meta:
+        verbose_name = 'верификацию'
+        verbose_name_plural = 'Верификация по почте'
